d1eeb780434701e95216b9e3c213d905d2590fa8
onlyhappiness <onlyhappiness96@gmail.com>
2023-01-03 21:48:24 +0900

    Add: tanstack Query

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,12 +1,17 @@
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import React from 'react';
 import {RecoilRoot} from 'recoil';
 import Navigation from './Navigation';
 
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
-    <RecoilRoot>
-      <Navigation />
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <Navigation />
+      </RecoilRoot>
+    </QueryClientProvider>
   );
 };
 
