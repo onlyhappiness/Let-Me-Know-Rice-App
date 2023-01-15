42af8756c0f0a14a879ae7e92e2808250b84c7c5
onlyhappiness <onlyhappiness96@gmail.com>
2023-01-15 19:13:07 +0900

    Feat: bottom tab에 icon 추가

--- src/stack/Tab.tsx
+++ src/stack/Tab.tsx
@@ -6,18 +6,55 @@ import Review from './Review';
 import Setting from './Setting';
 import Community from './Community';
 
+import IonIcon from 'react-native-vector-icons/Ionicons';
+
 export default () => {
   const Tab = useMemo(() => createBottomTabNavigator(), []);
 
   return (
     <Tab.Navigator
       initialRouteName="HomeStack"
-      screenOptions={{headerShown: false}}>
-      <Tab.Screen name="FavoriteStack" component={Favorite} />
-      <Tab.Screen name="ReviewStack" component={Review} />
-      <Tab.Screen name="HomeStack" component={Home} />
-      <Tab.Screen name="CommunityStack" component={Community} />
-      <Tab.Screen name="SettingStack" component={Setting} />
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color}) => {
+          let iconName = '';
+
+          if (route.name === 'FavoriteStack') {
+            iconName = 'heart-outline';
+          } else if (route.name === 'ReviewStack') {
+            iconName = 'chatbubble-ellipses-outline';
+          } else if (route.name === 'HomeStack') {
+            iconName = 'home-outline';
+          } else if (route.name === 'CommunityStack') {
+            iconName = 'chatbubbles-outline';
+          } else if (route.name === 'SettingStack') {
+            iconName = 'happy-outline';
+          }
+
+          return <IonIcon name={iconName} size={22} />;
+        },
+        headerShown: false,
+      })}>
+      <Tab.Screen
+        name="FavoriteStack"
+        component={Favorite}
+        options={{title: '찜'}}
+      />
+      <Tab.Screen
+        name="ReviewStack"
+        component={Review}
+        options={{title: '리뷰왕'}}
+      />
+      <Tab.Screen name="HomeStack" component={Home} options={{title: '홈'}} />
+      <Tab.Screen
+        name="CommunityStack"
+        component={Community}
+        options={{title: '커뮤니티'}}
+      />
+      <Tab.Screen
+        name="SettingStack"
+        component={Setting}
+        options={{title: '내 정보'}}
+      />
     </Tab.Navigator>
   );
 };
