0b4bb575ad72454c75294480a76502bfd00f51cf
onlyhappiness <onlyhappiness96@gmail.com>
2023-01-26 21:36:09 +0900

    Feat: 앱 종료 작업 중..

--- src/client/Navigation.tsx
+++ src/client/Navigation.tsx
@@ -1,4 +1,4 @@
-import React, {useMemo} from 'react';
+import React, {useEffect, useMemo} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Auth from '../stack/Auth';
@@ -10,6 +10,11 @@ export default () => {
   const userInfo = useRecoilValue(AuthState);
 
   const Stack = useMemo(() => createNativeStackNavigator(), []);
+
+  // TODO: 뒤로 가기 두번 클릭 시 앱 종료
+  // useEffect(() => {
+
+  // }, []);
 
   return (
     <NavigationContainer>
