ac9351caadf7139c2a2531d32d88177e20b79fe9
onlyhappiness <onlyhappiness96@gmail.com>
2023-01-29 22:20:14 +0900

    Feat: @notifee/react-native 추가

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,12 +1,38 @@
-import {View, Text, StyleSheet} from 'react-native';
-import React from 'react';
+import {View, Text, StyleSheet, Button} from 'react-native';
+import React, {useCallback} from 'react';
+import notifee from '@notifee/react-native';
 import HomeLayout from '../../layouts/HomeLayout';
 
 export default () => {
+  // TODO: notifee
+  const onDisplayNotification = useCallback(() => {
+    (async () => {
+      const channelId = await notifee.createChannel({
+        id: 'default',
+        name: 'Default Channel',
+      });
+
+      await notifee.displayNotification({
+        title: '밥줘',
+        body: '밥줘 알림',
+        android: {
+          channelId,
+        },
+      });
+    })();
+  }, []);
+
   return (
     <HomeLayout title="주소">
       <View style={styles.container}>
         <Text>Home</Text>
+
+        <Button
+          title="Display Notification"
+          onPress={() => {
+            onDisplayNotification();
+          }}
+        />
       </View>
     </HomeLayout>
   );
