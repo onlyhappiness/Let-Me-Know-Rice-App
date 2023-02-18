be91bb63c8eb7ddacd3a26c2bb1230427636e14b
onlyhappiness <onlyhappiness96@gmail.com>
2023-02-18 22:58:34 +0900

    Feat: 앱 종료 util로 분리

---
+++ src/util/quitApp.ts
@@ -0,0 +1,28 @@
+import {BackHandler} from 'react-native';
+import React, {useEffect, useRef} from 'react';
+import moment from 'moment';
+
+import SimpleToast from 'react-native-simple-toast';
+
+export default () => {
+  const ref = useRef();
+
+  const exit_time = useRef(0);
+
+  useEffect(() => {
+    const onBackPress = () => {
+      const canGoBack = ref.current?.canGoBack();
+      if (!canGoBack && exit_time.current < moment().unix() - 3) {
+        exit_time.current = moment().unix();
+        SimpleToast.show('한번 더 클릭시 종료합니다.', SimpleToast.SHORT);
+        return true;
+      } else if (!canGoBack) {
+        BackHandler.exitApp();
+      }
+    };
+    BackHandler.addEventListener('hardwareBackPress', onBackPress);
+    return () => {
+      BackHandler.removeEventListener('hardwareBackPress', onBackPress);
+    };
+  }, []);
+};
