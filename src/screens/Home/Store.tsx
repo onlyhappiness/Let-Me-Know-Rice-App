e24c99a9af614941e52da61948a70e3cbeeb17be
onlyhappiness <onlyhappiness96@gmail.com>
2023-04-16 22:07:38 +0900

    Feat: 가게 상세 screen 수정

--- src/screens/Home/Store.tsx
+++ src/screens/Home/Store.tsx
@@ -4,21 +4,24 @@ import {
   ScrollView,
   StyleSheet,
   Dimensions,
-  Image,
   TouchableOpacity,
+  ImageBackground,
 } from 'react-native';
 import React, {useState} from 'react';
 import {useGetStoreDetail} from '../../hooks/store.query';
 import {useMenu} from '../../hooks/menu.query';
 
 import {TabView, SceneMap, TabBar} from 'react-native-tab-view';
+import FeatherIcon from 'react-native-vector-icons/Feather';
 
 import {COLOR} from '../../theme/color';
+import Logo from '../../assets/logo.svg';
 
 import StoreAddress from '../../components/home/StoreAddress';
 import StoreInfo from '../../components/home/StoreInfo';
 import StoreMenu from '../../components/home/StoreMenu';
 import {useNavigation} from '@react-navigation/native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 const renderScene = SceneMap({
   first: StoreAddress,
@@ -30,6 +33,8 @@ const {width, height} = Dimensions.get('screen');
 
 export default ({route}: any) => {
   const navigation = useNavigation();
+
+  const {top} = useSafeAreaInsets();
 
   const {storeId} = route.params;
 
@@ -46,11 +51,21 @@ export default ({route}: any) => {
 
   return (
     <ScrollView contentContainerStyle={{flexGrow: 1}} style={{flex: 1}}>
-      <Image style={styles.image} />
-      {/* TODO: 이미지 안에 버튼 추가 */}
-      <TouchableOpacity style={styles.left} onPress={() => navigation.goBack()}>
-        <Text>나와</Text>
-      </TouchableOpacity>
+      <ImageBackground style={styles.image} source={{uri: storeDetail?.image}}>
+        <View style={[styles.imageHeader, {paddingTop: top}]}>
+          <TouchableOpacity
+            style={styles.left}
+            onPress={() => navigation.goBack()}>
+            <FeatherIcon name="arrow-left" size={24} color={'#ffffff'} />
+          </TouchableOpacity>
+          {storeDetail?.image ? null : (
+            <View style={styles.logo}>
+              <Logo width={200} />
+            </View>
+          )}
+          <View style={{flex: 1}} />
+        </View>
+      </ImageBackground>
 
       <View style={styles.centerBox}>
         <Text style={styles.storeName}>{storeDetail?.name}</Text>
@@ -93,6 +108,20 @@ const styles = StyleSheet.create({
     height: height / 3.5,
     backgroundColor: 'gray',
     marginBottom: 30,
+  },
+  imageHeader: {
+    padding: 10,
+    flexDirection: 'row',
+    width: '100%',
+  },
+  left: {
+    flex: 1,
+  },
+  logo: {
+    flex: 1,
+    height: height / 3.5,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   centerBox: {
     alignItems: 'center',
